a804e9f115b36efd59c4cd02c642736ffbe3cb11
Piotrek Wysocki <garry@aikido.art.pl>
2023-12-04 23:45:57 +0100

    load input as array

--- Tools/AbstractTask.php
+++ Tools/AbstractTask.php
@@ -18,6 +18,7 @@ abstract class AbstractTask implements TaskInterface
 
     public function taskResult(string $firstResult = null, string $secondResult = null)
     {
+        $this->output->newLine();
         $this->output->result([
             MessageEnum::HR,
             $firstResult ? sprintf(MessageEnum::RESULT_1, $this->input->getDay(), $firstResult) : null,
@@ -34,5 +35,17 @@ abstract class AbstractTask implements TaskInterface
     public function loadData(?string $filename = null): array
     {
         return $this->input->loadData($filename);
+    }
+    
+    /**
+     * @param string|null $filename If empty it will load default input/demo file
+     * @return array
+     * @throws Exception
+     */
+    public function loadDataAsArray(?string $filename = null): array
+    {
+        $data = $this->input->loadData($filename);
+        
+        return array_map(fn($row) => str_split(trim($row)), $data);
     }
 }
